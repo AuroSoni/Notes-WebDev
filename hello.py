9cadcc803730488a482e1c1236c0315bbdc0f9c8
Auro <aurokumar926@gmail.com>
2020-06-24 08:25:55 +0530

    importing without execution

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
-print("Enter your name.")
-name = input()
-print(f"Hello {name}!")
 def square(x):
     return x*x
-for i in range(10):
-    print("{} squared is {}".format(i+1,square(i+1)))
+
+def main():
+    print("Enter your name.")
+    name = input()
+    print(f"Hello {name}!")
+    for i in range(10):
+        print("{} squared is {}".format(i+1,square(i+1)))
+
+if __name__ == "__main__":
+    main()
+    #if I'm currently running this file
+    #only then execute the main() function.
